66db8160b3032d1b6dbea7626bfad3f3a34bfaa1
Felix Roos <flix91@gmail.com>
2023-07-17 11:40:54 +0200

    parse range

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,4 +1,38 @@
+const rangeParser = require("range-parser");
+
 exports.handler = async function (event, context) {
+  const rangeHeader = event.headers["range"];
+
+  if (rangeHeader) {
+    const ranges = rangeParser(audioFileSize, rangeHeader, { combine: true });
+    if (!ranges) {
+      return {
+        statusCode: 416,
+        body: "Requested Range Not Satisfiable",
+      };
+    } else if (ranges.type === "bytes") {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ ranges }),
+      };
+      /* const range = ranges[0];
+      const start = range.start;
+      const end = range.end;
+      const chunkSize = end - start + 1;
+      const fileStream = fs.createReadStream(audioFilePath, { start, end });
+      const head = {
+        "Content-Type": "audio/mpeg",
+        "Content-Length": chunkSize,
+        "Content-Range": `bytes ${start}-${end}/${audioFileSize}`,
+      };
+      console.log(head);
+      res.writeHead(206, "Partial Content", head);
+
+      fileStream.pipe(res);
+      return; */
+    }
+  }
+
   return {
     statusCode: 200,
     body: JSON.stringify({ event, context }),
